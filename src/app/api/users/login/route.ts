201254e2c1acc1b7a0a1c83e9445fcd07f7c9d27
naimuddin94 <developernaimuddin@gmail.com>
2024-04-19 15:25:25 +0600

    user login api create

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,50 @@
+import { connectDB } from "@/db/connectDB";
+import User from "@/models/user.model";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { NextRequest, NextResponse } from "next/server";
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    const passwordValid = await bcrypt.compare(password, user.password);
+
+    if (!passwordValid) {
+      return NextResponse.json(
+        { message: "Invalid credentials" },
+        { status: 403 }
+      );
+    }
+
+    const tokenPayload = {
+      _id: user._id,
+      email: user.email,
+      username: user.username,
+    };
+
+    const token = await jwt.sign(tokenPayload, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json(
+      { message: "Login successfully", success: true },
+      { status: 200 }
+    );
+
+    response.cookies.set("token", token, { httpOnly: true });
+
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
